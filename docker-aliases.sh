82d71ab8a400bbc0bfb810a8eed5c9c26fbe4948
Greerso <greerso@voskcoin.io>
2018-04-25 22:42:22 -0400

    cleanup

--- docker-aliases.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-function docker_alias() {
-    docker exec $1 gosu $2 $3
-}
-# bhash
-alias bhashdrestart="docker_alias greerso/bhashd bhash systemctl restart bhashd"
-alias bhash-cli="docker_alias greerso/bhashd bhash /usr/local/bin/bhash-cli"
